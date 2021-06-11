2e33d0eb14e3fd705f5ce3683123adb3f0f496fd
Boba Chef <bobagroup@protonmail.com>
2021-06-10 18:12:26 -0700

    ft(menu): add farm and pool to menu

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -21,78 +21,79 @@ const config: MenuEntry[] = [
       },
     ],
   },
-  /* 
+
   {
     label: 'Farms',
     icon: 'FarmIcon',
-    href: 'https://pancakeswap.finance/farms',
+    href: 'https://ricefarm.fi/farms',
   },
   {
     label: 'Pools',
     icon: 'PoolIcon',
-    href: 'https://pancakeswap.finance/pools',
+    href: 'https://ricefarm.fi/pools',
   },
-  {
-    label: 'Lottery',
-    icon: 'TicketIcon',
-    href: 'https://pancakeswap.finance/lottery',
-    status: {
-      text: 'WIN',
-      color: 'warning',
+  /*
+{
+  label: 'Lottery',
+  icon: 'TicketIcon',
+  href: 'https://pancakeswap.finance/lottery',
+  status: {
+    text: 'WIN',
+    color: 'warning',
+  },
+},
+{
+  label: 'Collectibles',
+  icon: 'NftIcon',
+  href: 'https://pancakeswap.finance/nft',
+},
+{
+  label: 'Team Battle',
+  icon: 'TeamBattleIcon',
+  href: 'https://pancakeswap.finance/competition',
+},
+
+{
+  label: 'Teams & Profile',
+  icon: 'GroupsIcon',
+  items: [
+    {
+      label: 'Leaderboard',
+      href: 'https://pancakeswap.finance/teams',
     },
-  },
-  {
-    label: 'Collectibles',
-    icon: 'NftIcon',
-    href: 'https://pancakeswap.finance/nft',
-  },
-  {
-    label: 'Team Battle',
-    icon: 'TeamBattleIcon',
-    href: 'https://pancakeswap.finance/competition',
-  },
-  
-  {
-    label: 'Teams & Profile',
-    icon: 'GroupsIcon',
-    items: [
-      {
-        label: 'Leaderboard',
-        href: 'https://pancakeswap.finance/teams',
-      },
-      {
-        label: 'Task Center',
-        href: 'https://pancakeswap.finance/profile/tasks',
-      },
-      {
-        label: 'Your Profile',
-        href: 'https://pancakeswap.finance/profile',
-      },
-    ],
-  },
-  {
-    label: 'Info',
-    icon: 'InfoIcon',
-    items: [
-      {
-        label: 'Overview',
-        href: 'https://pancakeswap.info',
-      },
-      {
-        label: 'Tokens',
-        href: 'https://pancakeswap.info/tokens',
-      },
-      {
-        label: 'Pairs',
-        href: 'https://pancakeswap.info/pairs',
-      },
-      {
-        label: 'Accounts',
-        href: 'https://pancakeswap.info/accounts',
-      },
-    ],
-  },
-   */
+    {
+      label: 'Task Center',
+      href: 'https://pancakeswap.finance/profile/tasks',
+    },
+    {
+      label: 'Your Profile',
+      href: 'https://pancakeswap.finance/profile',
+    },
+  ],
+},
+{
+  label: 'Info',
+  icon: 'InfoIcon',
+  items: [
+    {
+      label: 'Overview',
+      href: 'https://pancakeswap.info',
+    },
+    {
+      label: 'Tokens',
+      href: 'https://pancakeswap.info/tokens',
+    },
+    {
+      label: 'Pairs',
+      href: 'https://pancakeswap.info/pairs',
+    },
+    {
+      label: 'Accounts',
+      href: 'https://pancakeswap.info/accounts',
+    },
+  ],
+},
+ */
   {
     label: 'IFO',
     icon: 'IfoIcon',
